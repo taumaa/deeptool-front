b1ceea5a14d896b5104232db1a86a1a85e2b1664
tauma <contact@part.gg>
2023-06-22 12:54:04 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
 });
---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { defineMiddleware } from "astro/middleware";
+
+export const onRequest = defineMiddleware(async ({ request }, next) => {
+  // Check if the URL doesn't end with a slash and is not the root
+  if (!request.url.endsWith('/') && request.url !== '/') {
+    // Redirect to the same URL with a trailing slash
+    return new Response(null, {
+      status: 301,
+      headers: {
+        'Location': request.url + '/',
+      },
+    });
+  }
+
+  // Continue to the next middleware or route handler
+  return next();
+});
