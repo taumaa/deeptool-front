86baf5bcbb4be573a4807ed91dcf91ee1fbebe23
tauma <contact@part.gg>
2023-06-22 14:21:04 +0200

    Fix trailing slash

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,24 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware(async ({ request }, next) => {
-  // Check if the URL doesn't end with a slash and is not the root
-  if (!request.url.endsWith('/') && request.url !== '/') {
-    // Redirect to the same URL with a trailing slash
-    return new Response(null, {
-      status: 301,
-      headers: {
-        'Location': request.url + '/',
-      },
-    });
-  }
+
+    console.log(request.url)
+
+    if (request.url.endsWith('/sitemap.xml')) {
+        // Continue to the next middleware or route handler
+        return next();
+    }
+
+    // Check if the URL doesn't end with a slash and is not the root
+    if (!request.url.endsWith('/') && request.url !== '/') {
+        // Redirect to the same URL with a trailing slash
+        return new Response(null, {
+        status: 301,
+        headers: {
+            'Location': request.url + '/',
+        },
+        });
+    }
 
   // Continue to the next middleware or route handler
   return next();
